8aaa9763cb260a8c598c91654694396395fdcdc1
glendc <glen.decauwsemaecker@otainsight.com>
2020-08-22 23:16:30 +0200

    add init transpile function
    to simple to be true

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-exports.printMsg = function() {
-    console.log("This is a message from the demo package");
+const { parseScript: parseJS } = require('shift-parser');
+
+exports.transpile = function(script) {
+    if (typeof script === 'string' || script instanceof String) {
+        script = parseJS(script);
+    }
+    return script;
 }
