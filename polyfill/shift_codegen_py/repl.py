e328a24b607b4492c0c87551a8f9a7e52b899cfc
glendc <glen.decauwsemaecker@otainsight.com>
2020-09-04 00:31:04 +0200

    make python hot reload work with asterix

--- polyfill/shift_codegen_py/repl.py
+++ polyfill/shift_codegen_py/repl.py
@@ -4,13 +4,9 @@ import socketio
 import io
 import os
 import glob
-import importlib
 from aiohttp import web
 
 from pexpect import replwrap
-
-import polyfill
-from polyfill import *
 
 sio = socketio.AsyncServer()
 app = web.Application()
@@ -21,7 +17,9 @@ repl = replwrap.python()
 def push_and_read_eval(cmd):
   return repl.run_command(cmd)
 # TODO: make hot reloading work
+print(push_and_read_eval("import shift_codegen_py"))
 print(push_and_read_eval("from shift_codegen_py.polyfill import *"))
+print(push_and_read_eval("import importlib as PYTHON_STD_IMPORT_LIB"))
 
 polyfill_py_last_modified_time = 0
 polyfill_py_path_gob = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'polyfill', '*.py')
@@ -34,9 +32,18 @@ def hotreload_polyfill_library_if_desired():
   for polyfill_py_path in glob.glob(polyfill_py_path_gob):
     polyfill_py_last_modified_time_new = os.path.getmtime(polyfill_py_path)
     if polyfill_py_last_modified_time_new > polyfill_py_last_modified_time:
-        importlib.reload(polyfill)
-        polyfill_py_last_modified_time = polyfill_py_last_modified_time_new
-        break
+      cmds = [
+        "PYTHON_STD_IMPORT_LIB.reload(shift_codegen_py)",
+        "PYTHON_STD_IMPORT_LIB.reload(shift_codegen_py.polyfill)",
+        # TODO: make hot reload work with our asterix usage here...
+        "from shift_codegen_py.polyfill import *",
+      ]
+      for cmd in cmds:
+        r = push_and_read_eval(cmd)
+        if r and 'Exception' in r:
+          return r
+      polyfill_py_last_modified_time = polyfill_py_last_modified_time_new
+      break
   
 @sio.event
 def connect(sid, environ):
@@ -45,7 +52,9 @@ def connect(sid, environ):
 @sio.event
 def eval(sid, data):
   try:
-    hotreload_polyfill_library_if_desired()
+    es = hotreload_polyfill_library_if_desired()
+    if es:
+      return es
     return push_and_read_eval(data['cmd'])
   except Exception as e:
     return str(e)
